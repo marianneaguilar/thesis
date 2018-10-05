71123bf069da7ae79f327de985c1323cb8959f84
marianneaguilar <marianneaguilar@college.harvard.edu>
2018-10-04 20:37:50 -0400

    Add files via upload

--- clusterall.py
+++ clusterall.py
@@ -10,7 +10,7 @@ T=30
 P=N
 #Reorganize data into time series format without using pivot function
 data2=[]
-for i in list(set(data[:,9])):
+sfor i in list(set(data[:,9])):
     for col in of_interest:
         data_temp=data[i==data[:,9],col]
         data2.append(data_temp[0:29])
@@ -21,23 +21,23 @@ data2_per = []
 for pos in range(0,len(data2[:,1])-1):
     data2_per.append(periodogram(data2[pos,:]))
 #Calculate cross periodograms
-data2_cper=[]
-loc=0
-while loc < len(data2[:,1]):
-    for i in range(0,len(of_interest)-1):
-        for j in range(0,len(of_interest)-1):
-            data2_cper.append(spicy.signal.csd(data2[loc+i,:],data2[loc+j,:]))
-    loc=loc+len(of_interest)
+#data2_cper=[]
+#loc=0
+#while loc < len(data2[:,1]):
+ #   for i in range(0,len(of_interest)-1):
+  #      for j in range(0,len(of_interest)-1):
+   #         data2_cper.append(spicy.signal.csd(data2[loc+i,:],data2[loc+j,:]))
+    #loc=loc+len(of_interest)
 
 t2=len(data2_per[:,1])
 p2=len(data2_per[1,:])
-t3=len(data2_cper[:,1])
-p3=len(data2_cper[1,:])
+#t3=len(data2_cper[:,1])
+#p3=len(data2_cper[1,:])
 
 #Smooth periodograms and crossperiodograms
 m=10
 data2_per_sm=[]
-data2_cper_sm=[]
+#data2_cper_sm=[]
 for col in range(0,t2-1):
     if col < m:
         s = 0
@@ -55,22 +55,22 @@ for col in range(0,t2-1):
             s3=s3+sum(data[i,:])
         data2_per_sm.append(s3)
 
-for col in range(0,t3-1):
-    if col < m:
-        s = 0
-        for i in range(0,col+m):
-            s=s+sum(data[i,:])
-        data2_cper_sm.append(s)
-    else if col > (t3-1-m):
-        s2 = 0
-        for i in range(col-m,t3-1):
-            s2=s2+sum(data[i,:])
-        data2_cper_sm.append(s2)
-    else:
-        s3 = 0
-        for i in range(col-m,col+m):
-            s3=s3+sum(data[i,:])
-        data2_cper_sm.append(s3)
+#for col in range(0,t3-1):
+ #   if col < m:
+  #      s = 0
+   #     for i in range(0,col+m):
+    #        s=s+sum(data[i,:])
+     #   data2_cper_sm.append(s)
+    #else if col > (t3-1-m):
+     #   s2 = 0
+      #  for i in range(col-m,t3-1):
+       #     s2=s2+sum(data[i,:])
+        #data2_cper_sm.append(s2)
+    #else:
+     #   s3 = 0
+      #  for i in range(col-m,col+m):
+       #     s3=s3+sum(data[i,:])
+        #data2_cper_sm.append(s3)
 
 #Compute likelihood test for each frequency        
 Qxyw=[]
@@ -78,5 +78,11 @@ comparisons=[]
 for q in range(0,len(data2_per_sm[:,1])-2):
     for p in range(q+1,len(data2_per_sm[:,1])-1):
         comparisons.append(individuals[q]+"+"+individuals[p])
-        
-Z=linkage(X,'ward')
+        Q_new1=abs(data2_per_sm[:,p]**2)
+        Q_new2=abs(data2_per_sm[:,q]**2)
+        Q_new12=abs((data2_per_sm[:,p]+data2_per_sm[:,q])**2)
+        Q_new=(2**(2*2))*(Q_new1*Q_new2)/(Q_new12**2)
+        Qxyw.append(Q_new)
+Z=linkage(Qxyz,'ward')
+plt.title('Hierarchical Clustering Dendrogram of Multivariate Time Series')
+dendrogram(Z,labels=comparisons)
