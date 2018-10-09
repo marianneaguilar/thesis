38ad03a650c90a5bccbd3d4732b109fbb09d5236
marianneaguilar <marianneaguilar@college.harvard.edu>
2018-10-09 14:52:57 -0400

    Add files via upload

--- clusterall.py
+++ clusterall.py
@@ -20,30 +20,28 @@ for i in individuals:
             try:
                 data_temp2.append(float(data_temp[entry]))
             except:
-                a=0
-                c=0
-                if entry-14>=0:
+                if entry-14 >= 0:
                     try:
                         a=a+data_temp[entry-14]
                         c=c+1
                     except:
                         a=a+0
                         c=c+0
-                if entry-7>=0:
+                if entry-7 >= 0:
                     try:
                         a=a+data_temp[entry-7]
                         c=c+1
                     except:
                         a=a+0
                         c=c+0
-                if entry+7<len(temp_data2):
+                if entry+7 < len(data_temp):
                     try:
                         a=a+data_temp[entry+7]
                         c=c+1
                     except:
                         a=a+0
                         c=c+0
-                if entry+14<len(temp_data2):
+                if entry+14 < len(data_temp):
                     try:
                         a=a+data_temp[entry+14]
                         c=c+1
@@ -51,9 +49,9 @@ for i in individuals:
                         a=a+0
                         c=c+0
                 if c==0:
-                    temp_data2.append(numpy.mean(temp_data2))
+                    data_temp2.append(numpy.mean(data_temp))
                 else:
-                    temp_data2.append(a/c)
+                    data_temp2.append(a/c)
         data2.append(data_temp2[0:29])
 #Calculate periodogram-cross periodogram matrices
 data2_per = []
@@ -88,21 +86,32 @@ p3=len(data2_per_sm[1])
 
 #Compute likelihood test for each frequency        
 Qxyw=[]
-comparisons=t3/len(individuals)
-for person1 in range(0,p3-1):
-    determinants=[]
-    p=0
-    while p < len(data2_per_sm)-1):
-        s=0
-        comparisons.append(individuals[q]+"+"+individuals[p])
-        for freq in range(0,p2-1):
-            Q_new1=abs(data2_per_sm[p,freq]**2)
-            Q_new2=abs(data2_per_sm[q,freq]**2)
-            Q_new12=abs((data2_per_sm[p,freq]+data2_per_sm[q,freq])**2)
-            Q_new=(2**(2*2))*(Q_new1*Q_new2)/(Q_new12**2)
-            s=s+Q_new
-        Qxyw.append(s/p2)
-Z=linkage(Qxyw,'single',optimal_ordering=True)
-plt.title('Hierarchical Clustering Dendrogram of Multivariate Time Series')
-dendrogram(Z,labels=individuals)
-dendrogram(Z,truncate_mode='lastp',p=6,show_contracted=True)
+comparisons=int(t3/len(individuals))
+data2_per_sm_usable=numpy.transpose(data2_per_sm)
+for col in range(0,p3):
+    temp_col=data2_per_sm_usable[col]
+    person1=0
+    while person1 < t3:
+        reorganize1=[]
+        r1=0
+        while r1 < comparisons:
+            reorganize1.append(temp_col[person1+r1:person1+r1+len(of_interest)])
+            r1=r1+len(of_interest)
+        person2=person1+comparisons
+        while person2 < t3:
+            reorganize2=[]
+            r2=0
+            while r2 < comparisons:
+                reorganize2.append(temp_col[person2+r2:person2+r2+len(of_interest)])
+                r2=r2+len(of_interest)
+            Q_new1=abs(numpy.linalg.det(reorganize1))**(2*m+1)
+            Q_new2=abs(numpy.linalg.det(reorganize2))**(2*m+1)
+            Q_new12=abs(numpy.linalg.det(numpy.matrix(reorganize1)+numpy.matrix(reorganize2)))**(2*m+1)
+            Q_new=(2**(2*(2*m+1)))*(Q_new1*Q_new2)/(Q_new12**2)
+            Qxyw.append(Q_new)
+            person2=person2+comparisons
+        person1=person1+comparisons
+#Z=linkage(Qxyw,'single',optimal_ordering=True)
+#plt.title('Hierarchical Clustering Dendrogram of Multivariate Time Series')
+#dendrogram(Z,labels=individuals)
+#dendrogram(Z,truncate_mode='lastp',p=6,show_contracted=True)
