0c2e2046bab222197e02e1042e65021eb4d54e17
marianneaguilar <marianneaguilar@college.harvard.edu>
2019-03-26 09:34:11 -0400

    Add files via upload

--- graphthesis.m
+++ graphthesis.m
@@ -1,21 +1,196 @@
+%% Create graph for cluster 1 of all variable relationships, negative and positive relationships
 d0=graph(group0vars,ofinterestnames,'omitselfloops');
 h0=graph(group0high,ofinterestnames,'omitselfloops');
+j0=graph(group0pos,ofinterestnames,'omitselfloops');
 p0=plot(d0,'Layout','force','UseGravity','on')
 highlight(p0,h0,'EdgeColor','r')
+p=p0.NodeLabel;
+p0.NodeLabel='';
+text(get(p0,'XData'),get(p0,'YData'),p,'FontSize',12,'FontWeight','bold','HorizontalAlignment','left','VerticalAlignment','middle')
 cla;
+%% Create graph for cluster 2 of all variable relationships, negative and positive relationships
 d1=graph(group1vars,ofinterestnames,'omitselfloops');
 h1=graph(group1high,ofinterestnames,'omitselfloops');
+j1=graph(group1pos,ofinterestnames,'omitselfloops');
 p1=plot(d1,'Layout','force','UseGravity','on')
 highlight(p1,h1,'EdgeColor','r')
+p=p1.NodeLabel;
+p1.NodeLabel='';
+text(get(p1,'XData'),get(p1,'YData'),p,'FontSize',12,'FontWeight','bold','HorizontalAlignment','left','VerticalAlignment','middle')
 cla;
+%% Create graph for cluster 3 of all variable relationships, negative and positive relationships
 d2=graph(group2vars,ofinterestnames,'omitselfloops');
 h2=graph(group2high,ofinterestnames,'omitselfloops');
+j2=graph(group2pos,ofinterestnames,'omitselfloops');
 p2=plot(d2,'Layout','force','UseGravity','on')
 highlight(p2,h2,'EdgeColor','r')
+p=p2.NodeLabel;
+p2.NodeLabel='';
+text(get(p2,'XData'),get(p2,'YData'),p,'FontSize',12,'FontWeight','bold','HorizontalAlignment','left','VerticalAlignment','middle')
 cla;
+%% Create graph for cluster 4 of all variable relationships, negative and positive relationships
 d3=graph(group3vars,ofinterestnames,'omitselfloops');
 h3=graph(group3high,ofinterestnames,'omitselfloops');
+j3=graph(group3pos,ofinterestnames,'omitselfloops');
 p3=plot(d3,'Layout','force','UseGravity','on')
 highlight(p3,h3,'EdgeColor','r')
+p=p3.NodeLabel;
+p3.NodeLabel='';
+text(get(p3,'XData'),get(p3,'YData'),p,'FontSize',12,'FontWeight','bold','HorizontalAlignment','left','VerticalAlignment','middle')
 cla;
 
+%% Create directed graph (not for display) of day-variable relationships for cluster 1
+tempgraph=digraph(group0comphigh,dayvarpair,'omitselfloops');
+%% Find degree and centrality of each day-variable
+degr=outdegree(tempgraph);
+cen0=centrality(tempgraph,'betweenness');
+%% Repeat for cluster 2
+tempgraph2=digraph(group1comphigh,dayvarpair,'omitselfloops');
+degr2=outdegree(tempgraph2);
+cen1=centrality(tempgraph2,'betweenness');
+%% Repeat for cluster 3
+tempgraph3=digraph(group2comphigh,dayvarpair,'omitselfloops');
+degr3=outdegree(tempgraph3);
+cen2=centrality(tempgraph3,'betweenness');
+%% Repeat for cluster 4
+tempgraph4=digraph(group3comphigh,dayvarpair,'omitselfloops');
+degr4=outdegree(tempgraph4);
+cen3=centrality(tempgraph4,'betweenness');
+
+%% Find average measure of centrality
+avg0=(degr+cen0)/2;
+avg1=(degr2+cen1)/2;
+avg2=(degr3+cen2)/2;
+avg3=(degr4+cen3)/2;
+
+%% Nodes with maximal centrality measures (i.e. central hubs)
+max0=[200 201 204 208 817];
+max1=[201 416 420 422 817];
+max2=[201 415 416 661 817];
+max3=[201 202 661 686 817];
+
+%% Matrix to find shortest paths
+group0compfinal=zeros(1215,5);
+group1compfinal=zeros(1215,5);
+group2compfinal=zeros(1215,5);
+group3compfinal=zeros(1215,5);
+
+%% Find shortest path from each central hub to every other node and calculate projected effect for 0.9 retention
+for i=1:length(max0)
+    r1=max0(i);
+    r2=max1(i);
+    r3=max2(i);
+    r4=max3(i);
+    for j=1:1215
+        v=shortestpath(tempgraph,r1,j,'Method','unweighted');
+        s=1;
+        for k=2:length(v)
+            s=s*abs(group0comphigh(v(k-1),v(k)))/group0comphigh(v(k-1),v(k));
+        end
+        t=s*(0.9)^(length(v)-1);
+        if abs(t)<1
+            group0compfinal(j,i)=t;
+        end
+        v1=shortestpath(tempgraph2,r2,j,'Method','unweighted');
+        s=1;
+        for k=2:length(v1)
+            s=s*abs(group1comphigh(v1(k-1),v1(k)))/group1comphigh(v1(k-1),v1(k));
+        end
+        t=s*(0.9)^(length(v1)-1);
+        if abs(t)<1
+            group1compfinal(j,i)=t;
+        end
+        v2=shortestpath(tempgraph3,r3,j,'Method','unweighted');
+        s=1;
+        for k=2:length(v2)
+            s=s*abs(group2comphigh(v2(k-1),v2(k)))/group2comphigh(v2(k-1),v2(k));
+        end
+        t=s*(0.9)^(length(v2)-1);
+        if abs(t)<1
+            group2compfinal(j,i)=t;
+        end
+        v3=shortestpath(tempgraph4,r4,j,'Method','unweighted');
+        s=1;
+        for k=2:length(v3)
+            s=s*abs(group3comphigh(v3(k-1),v3(k)))/group3comphigh(v3(k-1),v3(k));
+        end
+        t=s*(0.9)^(length(v3)-1);
+        if abs(t)<1
+            group3compfinal(j,i)=t;
+        end
+    end
+end
+
+%% Import the weight matrices from Python for cluster 1
+    [~, ~, raw] = xlsread(strcat('/Users/marianneaguilar/Documents/4exampleidentity.xlsx'),'Sheet1');
+    raw = raw(2:end,2:end);
+    %% Create output variable
+    exampleidentity = reshape([raw{:}],size(raw));
+    %% Clear temporary variables
+    clearvars raw;
+    [~, ~, raw] = xlsread(strcat('/Users/marianneaguilar/Documents/4example2identity.xlsx'),'Sheet2');
+    raw = raw(2:end,2:end);
+    example2identity = reshape([raw{:}],size(raw));
+    clearvars raw;
+    [~, ~, raw] = xlsread(strcat('/Users/marianneaguilar/Documents/4example3identity.xlsx'),'Sheet3');
+    raw = raw(2:end,2:end);
+    example3identity = reshape([raw{:}],size(raw));
+    clearvars raw;
+    [~, ~, raw] = xlsread(strcat('/Users/marianneaguilar/Documents/4example4identity.xlsx'),'Sheet4');
+    raw = raw(2:end,2:end);
+    example4identity = reshape([raw{:}],size(raw));
+    clearvars raw;
+    
+%% Import the weight matrices from Python for cluster 2
+    [~, ~, raw] = xlsread(strcat('/Users/marianneaguilar/Documents/4exampleidentity2ndlayer.xlsx'),'Sheet1');
+    raw = raw(2:end,2:end);
+    %% Create output variable
+    exampleidentity2ndlayer = reshape([raw{:}],size(raw));
+    %% Clear temporary variables
+    clearvars raw;
+    [~, ~, raw] = xlsread(strcat('/Users/marianneaguilar/Documents/4example2identity2ndlayer.xlsx'),'Sheet2');
+    raw = raw(2:end,2:end);
+    example2identity2ndlayer = reshape([raw{:}],size(raw));
+    clearvars raw;
+    [~, ~, raw] = xlsread(strcat('/Users/marianneaguilar/Documents/4example3identity2ndlayer.xlsx'),'Sheet3');
+    raw = raw(2:end,2:end);
+    example3identity2ndlayer = reshape([raw{:}],size(raw));
+    clearvars raw;
+    [~, ~, raw] = xlsread(strcat('/Users/marianneaguilar/Documents/4example4identity2ndlayer.xlsx'),'Sheet4');
+    raw = raw(2:end,2:end);
+    example4identity2ndlayer = reshape([raw{:}],size(raw));
+    clearvars raw;
+    
+%% Import the weight matrices from Python for cluster 3
+    [~, ~, raw] = xlsread(strcat('/Users/marianneaguilar/Documents/4exampleidentity3rdlayer.xlsx'),'Sheet1');
+    raw = raw(2:end,2:end);
+    %% Create output variable
+    exampleidentity3rdlayer = reshape([raw{:}],size(raw));
+    %% Clear temporary variables
+    clearvars raw;
+    [~, ~, raw] = xlsread(strcat('/Users/marianneaguilar/Documents/4example2identity3rdlayer.xlsx'),'Sheet2');
+    raw = raw(2:end,2:end);
+    example2identity3rdlayer = reshape([raw{:}],size(raw));
+    clearvars raw;
+    [~, ~, raw] = xlsread(strcat('/Users/marianneaguilar/Documents/4example3identity3rdlayer.xlsx'),'Sheet3');
+    raw = raw(2:end,2:end);
+    example3identity3rdlayer = reshape([raw{:}],size(raw));
+    clearvars raw;
+    [~, ~, raw] = xlsread(strcat('/Users/marianneaguilar/Documents/4example4identity3rdlayer.xlsx'),'Sheet4');
+    raw = raw(2:end,2:end);
+    example4identity3rdlayer = reshape([raw{:}],size(raw));
+    clearvars raw;
+    
+%% Calculate net effect of one standard deviation change on three mood measures using weight matrices and distances    
+neteffect=zeros(5,3);
+neteffect2=zeros(5,3);
+neteffect3=zeros(5,3);
+neteffect4=zeros(5,3);
+for k=1:5
+    for j=1:3
+        neteffect(k,j)=sum(transpose(exampleidentity3rdlayer(:,j))*(transpose(exampleidentity2ndlayer)*(transpose(exampleidentity)*group0compfinal(:,k))));
+        neteffect2(k,j)=sum(transpose(example2identity3rdlayer(:,j))*(transpose(example2identity2ndlayer)*(transpose(example2identity)*group1compfinal(:,k))));
+        neteffect3(k,j)=sum(transpose(example3identity3rdlayer(:,j))*(transpose(example3identity2ndlayer)*(transpose(example3identity)*group2compfinal(:,k))));
+        neteffect4(k,j)=sum(transpose(example4identity2ndlayer(:,j))*(transpose(example4identity2ndlayer)*(transpose(example4identity)*group3compfinal(:,k))));
+    end
+end
--- thesis.m
+++ thesis.m
@@ -1,45 +1,59 @@
+%% Create matrices that will hold relationship between each day-variable
+%% pair for each cluster
 group0comp=zeros(1215,1215);
 group1comp=zeros(1215,1215);
 group2comp=zeros(1215,1215);
 group3comp=zeros(1215,1215);
+%% Create vector that will hold largest contributions to hidden layers
+%% overall for each cluster
+group0contr=zeros(1215,1);
+group1contr=zeros(1215,1);
+group2contr=zeros(1215,1);
+group3contr=zeros(1215,1);
+%% Find each iteration of the bootstrap data released from Python
 for n=0:98
-    %% Import the data
+    %%Import the data
     [~, ~, raw] = xlsread(strcat('/Users/marianneaguilar/Documents/4exampleidentityiteration',num2str(n),'.xlsx'),'Sheet1');
     raw = raw(2:end,2:end);
-    %% Create output variable
+    %%Create output variable
     exampleidentity = reshape([raw{:}],size(raw));
-    %% Clear temporary variables
+    %%Clear temporary variables
     clearvars raw;
+    %%Repeat for second cluster
     [~, ~, raw] = xlsread(strcat('/Users/marianneaguilar/Documents/4example2identityiteration',num2str(n),'.xlsx'),'Sheet2');
     raw = raw(2:end,2:end);
     example2identity = reshape([raw{:}],size(raw));
     clearvars raw;
+    %%Repeat for third cluster
     [~, ~, raw] = xlsread(strcat('/Users/marianneaguilar/Documents/4example3identityiteration',num2str(n),'.xlsx'),'Sheet3');
     raw = raw(2:end,2:end);
     example3identity = reshape([raw{:}],size(raw));
     clearvars raw;
+    %%Repeat for fourth cluster
     [~, ~, raw] = xlsread(strcat('/Users/marianneaguilar/Documents/4example4identityiteration',num2str(n),'.xlsx'),'Sheet4');
     raw = raw(2:end,2:end);
     example4identity = reshape([raw{:}],size(raw));
     clearvars raw;
+    %%For each day-variable pair, calculate the ratio of the dot product to
+    %%the maximal possible dot product
     for i=0:1214
+        v1=exampleidentity(i+1,:);
+        v3=example2identity(i+1,:);
+        v5=example3identity(i+1,:);
+        v7=example4identity(i+1,:);
         for j=0:1214
             comp1=mod(i,27);
             comp2=mod(j,27);
             if comp1<=comp2
-                v1=exampleidentity(i+1,:);
                 v2=exampleidentity(j+1,:);
                 v1abs=abs(v1);
                 v2abs=abs(v2);
-                v3=example2identity(i+1,:);
                 v4=example2identity(j+1,:);
                 v3abs=abs(v3);
                 v4abs=abs(v4);
-                v5=example3identity(i+1,:);
                 v6=example3identity(j+1,:);
                 v5abs=abs(v5);
                 v6abs=abs(v6);
-                v7=example4identity(i+1,:);
                 v8=example4identity(j+1,:);
                 v7abs=abs(v7);
                 v8abs=abs(v8);
@@ -49,21 +63,37 @@ for n=0:98
                 group3comp(i+1,j+1)=group3comp(i+1,j+1)+dot(v7,v8)/dot(v7abs,v8abs);
             end
         end
+        %%Add dot products to find contribution
+        group0contr(i+1)=group0contr(i+1)+dot(v1,v1);
+        group1contr(i+1)=group1contr(i+1)+dot(v3,v3);
+        group2contr(i+1)=group2contr(i+1)+dot(v5,v5);
+        group3contr(i+1)=group3contr(i+1)+dot(v7,v7);
     end
 end
-group0comp=group0comp/100;
-group1comp=group1comp/100;
-group2comp=group2comp/100;
-group3comp=group3comp/100;
 
+%% Take average of students dot product calculation
+group0comp=group0comp/99;
+group1comp=group1comp/99;
+group2comp=group2comp/99;
+group3comp=group3comp/99;
+
+%% Create matrix for averaged relationships for each variable for each cluster
 group0vars=zeros(45,45);
 group1vars=zeros(45,45);
 group2vars=zeros(45,45);
 group3vars=zeros(45,45);
+
+%% Create matrix for negative averaged relationships for each variable for each cluster
 group0high=zeros(45,45);
 group1high=zeros(45,45);
 group2high=zeros(45,45);
 group3high=zeros(45,45);
+
+%% Create matrix for positive averaged relationships for each variable for each cluster
+group0pos=zeros(45,45);
+group1pos=zeros(45,45);
+group2pos=zeros(45,45);
+group3pos=zeros(45,45);
 for i=0:44
     for j=0:44
         begin=i*27+1;
@@ -77,22 +107,31 @@ for i=0:44
         group0vars(i+1,j+1)=det0;
         if det0<0
             group0high(i+1,j+1)=det0;
+        else
+            group0pos(i+1,j+1)=det0;
         end
         group1vars(i+1,j+1)=det1;
         if det1<0
             group1high(i+1,j+1)=det1;
+        else
+            group1pos(i+1,j+1)=det1;
         end
         group2vars(i+1,j+1)=det2;
         if det2<0
             group2high(i+1,j+1)=det2;
+        else
+            group2pos(i+1,j+1)=det2;
         end
         group3vars(i+1,j+1)=det3;
         if det3<0
             group3high(i+1,j+1)=det3;
+        else
+            group3pos(i+1,j+1)=det3;
         end
     end
 end
 
+%% Find maximum and minimal values for variables
 temp0=reshape(group0vars,[2025,1]);
 temp1=reshape(group1vars,[2025,1]);
 temp2=reshape(group2vars,[2025,1]);
@@ -105,43 +144,80 @@ bot0=max(mink(temp0,200));
 bot1=max(mink(temp1,200));
 bot2=max(mink(temp2,200));
 bot3=max(mink(temp3,200));
+
+%% Select for highest and lowest 200 values
 for i=1:45
     for j=1:45
         if group0vars(i,j)<top0 & group0vars(i,j)>bot0
             group0vars(i,j)=0;
             group0high(i,j)=0;
+            group0pos(i,j)=0;
         end
         if group1vars(i,j)<top1 & group1vars(i,j)>bot1
             group1vars(i,j)=0;
             group1high(i,j)=0;
+            group1pos(i,j)=0;
         end
         if group2vars(i,j)<top2 & group2vars(i,j)>bot2
             group2vars(i,j)=0;
             group2high(i,j)=0;
+            group2pos(i,j)=0;
         end
         if group3vars(i,j)<top3 & group3vars(i,j)>bot3
             group3vars(i,j)=0;
             group3high(i,j)=0;
+            group3pos(i,j)=0;
         end
     end
 end
 
-%% Import data from spreadsheet
-% Script for importing data from the following spreadsheet:
-%
-%    Workbook: /Users/marianneaguilar/Documents/ofinterestnames.xlsx
-%    Worksheet: Sheet1
-%
-% To extend the code for use with different selected data or a different
-% spreadsheet, generate a function instead of a script.
+%% Find maximum and minimal values for day-variables
+temp0=reshape(group0comp,[1215*1215,1]);
+temp1=reshape(group1comp,[1215*1215,1]);
+temp2=reshape(group2comp,[1215*1215,1]);
+temp3=reshape(group3comp,[1215*1215,1]);
+top0=min(maxk(temp0,15915));
+top1=min(maxk(temp1,15915));
+top2=min(maxk(temp2,15915));
+top3=min(maxk(temp3,15915));
+bot0=max(mink(temp0,14700));
+bot1=max(mink(temp1,14700));
+bot2=max(mink(temp2,14700));
+bot3=max(mink(temp3,14700));
+group0comphigh=zeros(1215,1215);
+group1comphigh=zeros(1215,1215);
+group2comphigh=zeros(1215,1215);
+group3comphigh=zeros(1215,1215);
 
-% Auto-generated by MATLAB on 2019/02/06 19:47:01
+%% Select for highest and lowest values
+for i=1:1215
+    for j=1:1215
+        if group0comp(i,j)>=top0 || group0comp(i,j)<=bot0
+            group0comphigh(i,j)=group0comp(i,j);
+        end
+        if group1comp(i,j)>=top1 || group1comp(i,j)<=bot1
+            group1comphigh(i,j)=group1comp(i,j);
+        end
+        if group2comp(i,j)>=top2 || group2comp(i,j)<=bot2
+            group2comphigh(i,j)=group2comp(i,j);
+        end
+        if group3comp(i,j)>=top3 || group3comp(i,j)<=bot3
+            group3comphigh(i,j)=group3comp(i,j);
+        end
+    end
+end
 
-%% Import the data
+%% Import the data for variable names
 [~, ~, ofinterestnames] = xlsread('/Users/marianneaguilar/Documents/ofinterestnames.xlsx','Sheet1');
 ofinterestnames = ofinterestnames(2:end,end);
 ofinterestnames(cellfun(@(x) ~isempty(x) && isnumeric(x) && isnan(x),ofinterestnames)) = {''};
+dayvarpair=cell(1214,1);
+
+%% Improve string to remove spaces or dashes
 for i=1:length(ofinterestnames)
     ofinterestnames(i)=strrep(ofinterestnames(i),'_',' ');
+    for j=1:27
+        dayvarpair((i-1)*27+j)=strcat(ofinterestnames(i),' on day ',num2str(j));
+    end
 end
 
